956009c2cfed618ff11198461dde947c6418e22b
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-01 23:57:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,16 @@
 class Anagram
   
+  #set the readers/writers for my instance variables 
+attr_accessor :word
+
+  #take a word on initialization
+def initialize(word)
+  @word = word 
+end 
+
+#array of possible anagrams and returns all matches in an array || empty array
+def match(possible_anagrams)
+  
+end 
   
 end 
